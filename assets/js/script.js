9416f3b069bb5ba31997c5940fad68424e7f817c
ABIJITH <ABIJITHSAJU3336@GMAIL.COM>
2023-04-05 15:35:29 +0530

    solved conflict

--- assets/js/script.js
+++ assets/js/script.js
@@ -74,3 +74,46 @@ for (let i = 0; i < whishlistBtns.length; i++) {
     toggleElem(this);
   });
 }
+
+
+
+// aboutus js
+
+$(".tab_content").hide();
+$(".tab_content:first").show();
+
+/* if in tab mode */
+$("ul.tabs li").click(function() {
+
+  $(".tab_content").hide();
+  var activeTab = $(this).attr("rel"); 
+  $("#"+activeTab).fadeIn();		
+
+  $("ul.tabs li").removeClass("active");
+  $(this).addClass("active");
+
+$(".tab_drawer_heading").removeClass("d_active");
+$(".tab_drawer_heading[rel^='"+activeTab+"']").addClass("d_active");
+
+});
+/* if in drawer mode */
+$(".tab_drawer_heading").click(function() {
+  
+  $(".tab_content").hide();
+  var d_activeTab = $(this).attr("rel"); 
+  $("#"+d_activeTab).fadeIn();
+
+$(".tab_drawer_heading").removeClass("d_active");
+  $(this).addClass("d_active");
+
+$("ul.tabs li").removeClass("active");
+$("ul.tabs li[rel^='"+d_activeTab+"']").addClass("active");
+});
+
+
+/* Extra class "tab_last" 
+ to add border to right side
+ of last tab */
+$('ul.tabs li').last().addClass("tab_last");
+
+// aboutus js 
